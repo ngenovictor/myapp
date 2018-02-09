685b5a9ec2ffd715aac66f90b32a8eaef1691321
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2018-02-09 17:19:12 +0300

    remove post csrf

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from django.shortcuts import render
 
 # Create your views here.
+@csrf_exempt
 def home(request):
     print(request)
     return HttpResponse("hi")
