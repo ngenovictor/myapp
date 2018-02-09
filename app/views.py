b09c2605378d45e1b6c8d2f17342a8bd5e68c12f
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2018-02-09 17:22:27 +0300

    add proper formatting for the request

--- app/views.py
+++ app/views.py
@@ -8,5 +8,24 @@ from django.shortcuts import render
 # Create your views here.
 @csrf_exempt
 def home(request):
-    print(request)
+    headers = ''
+    for header, value in request.META.items():
+        if not header.startswith('HTTP'):
+            continue
+        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+        headers += '{}: {}\n'.format(header, value)
+
+    print( (
+        '{method} HTTP/1.1\n'
+        'Content-Length: {content_length}\n'
+        'Content-Type: {content_type}\n'
+        '{headers}\n\n'
+        '{body}'
+    ).format(
+        method=request.method,
+        content_length=request.META['CONTENT_LENGTH'],
+        content_type=request.META['CONTENT_TYPE'],
+        headers=headers,
+        body=request.body,
+    ))
     return HttpResponse("hi")
